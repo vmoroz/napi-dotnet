cb6ec60614b47b31f3d00990371584dfc71ae332
Vladimir Morozov <vmoroz@users.noreply.github.com>
2022-12-23 11:24:02 -0800

    Add String interface

--- Runtime/TypeScriptModel.cs
+++ Runtime/TypeScriptModel.cs
@@ -1,12 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Diagnostics.Metrics;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace NodeApi.EcmaScript;
 
@@ -276,7 +268,7 @@ public struct Intersect<T1, T2>
     public static implicit operator JSValue(Intersect<T1, T2> value) => value._value;
 }
 
-public struct Readonly<T> : IUnknown<Readonly<T>>
+public struct Readonly<T> : IJSValueHolder<Readonly<T>>
 {
     private JSValue _value;
 
@@ -284,13 +276,13 @@ public struct Readonly<T> : IUnknown<Readonly<T>>
     public static implicit operator JSValue(Readonly<T> value) => value._value;
 }
 
-public interface IUnknown<TSelf> where TSelf : IUnknown<TSelf>
+public interface IJSValueHolder<TSelf> where TSelf : IJSValueHolder<TSelf>
 {
     public static abstract explicit operator TSelf(JSValue value);
     public static abstract implicit operator JSValue(TSelf value);
 }
 
-public interface IFunction<TSelf> : IUnknown<TSelf>
+public interface IFunction<TSelf> : IJSValueHolder<TSelf>
     where TSelf : IFunction<TSelf>
 {
 }
@@ -369,7 +361,7 @@ public struct ObjectConstructor
      */
     // defineProperty<T>(o: T, p: PropertyKey, attributes: PropertyDescriptor & ThisType<any>): T;
     public T DefineProperty<T>(T value, PropertyKey key, Intersect<PropertyDescriptor, ThisType<Any>> attributes)
-        where T : struct, IUnknown<T>
+        where T : struct, IJSValueHolder<T>
         => (T)_value.CallMethod(NameTable.defineProperty, value, key, attributes);
     /**
      * Adds one or more properties to an object, and/or modifies attributes of existing properties.
@@ -378,7 +370,7 @@ public struct ObjectConstructor
      */
     // defineProperties<T>(o: T, properties: PropertyDescriptorMap & ThisType<any>): T;
     public T DefineProperties<T>(T value, Intersect<PropertyDescriptorMap, ThisType<Any>> properties)
-        where T : struct, IUnknown<T>
+        where T : struct, IJSValueHolder<T>
         => (T)_value.CallMethod(NameTable.defineProperties, value, properties);
 
     /**
@@ -387,7 +379,7 @@ public struct ObjectConstructor
      */
     // seal<T>(o: T): T;
     public T Seal<T>(T value)
-        where T : struct, IUnknown<T>
+        where T : struct, IJSValueHolder<T>
         => (T)_value.CallMethod(NameTable.seal, value);
 
     /**
@@ -411,7 +403,7 @@ public struct ObjectConstructor
      */
     // freeze<T>(o: T): Readonly<T>;
     public Readonly<T> Freeze<T>(T value)
-        where T : struct, IUnknown<T>
+        where T : struct, IJSValueHolder<T>
         => (Readonly<T>)_value.CallMethod(NameTable.freeze, value);
 
     /**
@@ -420,7 +412,7 @@ public struct ObjectConstructor
      */
     // preventExtensions<T>(o: T): T;
     public T PreventExtensions<T>(T value)
-        where T : struct, IUnknown<T>
+        where T : struct, IJSValueHolder<T>
         => (T)_value.CallMethod(NameTable.preventExtensions, value);
 
     /**
@@ -461,7 +453,7 @@ public struct ObjectConstructor
  * buffer as needed.
  */
 // interface ArrayBuffer
-public struct ArrayBuffer : IUnknown<ArrayBuffer>
+public struct ArrayBuffer : IJSValueHolder<ArrayBuffer>
 {
     private JSValue _value;
 
@@ -495,6 +487,290 @@ public partial struct Global
         set => _value.SetProperty(NameTable.Object, value);
     }
 }
+
+//interface String
+public partial interface IString<TSelf> : IJSValueHolder<TSelf>
+    where TSelf : IString<TSelf>
+{
+    /** Returns a string representation of a string. */
+    // toString(): string;
+    String toString();
+
+    /**
+     * Returns the character at the specified index.
+     * @param pos The zero-based index of the desired character.
+     */
+    // charAt(pos: number): string;
+    String charAt(Number index);
+
+    /**
+     * Returns the Unicode value of the character at the specified location.
+     * @param index The zero-based index of the desired character. If there is no character at the specified index, NaN is returned.
+     */
+    // charCodeAt(index: number): number;
+    String charCodeAt(Number index);
+
+    /**
+     * Returns a string that contains the concatenation of two or more strings.
+     * @param strings The strings to append to the end of the string.
+     */
+    String concat(params String[] strings);
+
+    /**
+     * Returns the position of the first occurrence of a substring.
+     * @param searchString The substring to search for in the string
+     * @param position The index at which to begin searching the String object. If omitted, search starts at the beginning of the string.
+     */
+    // indexOf(searchString: string, position?: number): number;
+    Number indexOf(String searchString, Number? position);
+
+    /**
+     * Returns the last occurrence of a substring in the string.
+     * @param searchString The substring to search for.
+     * @param position The index at which to begin searching. If omitted, the search begins at the end of the string.
+     */
+    // lastIndexOf(searchString: string, position?: number): number;
+    Number lastIndexOf(String searchString, Number? position);
+
+    /**
+     * Determines whether two strings are equivalent in the current locale.
+     * @param that String to compare to target string
+     */
+    // localeCompare(that: string): number;
+    Number localeCompare(String value);
+
+    /**
+     * Matches a string with a regular expression, and returns an array containing the results of that search.
+     * @param regexp A variable name or string literal containing the regular expression pattern and flags.
+     */
+    // match(regexp: string | RegExp): RegExpMatchArray | null;
+    //TODO:Nullable<RegExpMatchArray> match(Or<String, RegExp> regexp);
+
+    /**
+     * Replaces text in a string, using a regular expression or search string.
+     * @param searchValue A string or regular expression to search for.
+     * @param replaceValue A string containing the text to replace. When the {@linkcode searchValue} is a `RegExp`, all matches are replaced if the `g` flag is set (or only those matches at the beginning, if the `y` flag is also present). Otherwise, only the first match of {@linkcode searchValue} is replaced.
+     */
+    // replace(searchValue: string | RegExp, replaceValue: string): string;
+    //TODO:
+
+    /**
+     * Replaces text in a string, using a regular expression or search string.
+     * @param searchValue A string to search for.
+     * @param replacer A function that returns the replacement text.
+     */
+    // replace(searchValue: string | RegExp, replacer: (substring: string, ...args: any[]) => string): string;
+    //TODO:
+
+    /**
+     * Finds the first substring match in a regular expression search.
+     * @param regexp The regular expression pattern and applicable flags.
+     */
+    // search(regexp: string | RegExp): number;
+    //TODO:
+
+    /**
+     * Returns a section of a string.
+     * @param start The index to the beginning of the specified portion of stringObj.
+     * @param end The index to the end of the specified portion of stringObj. The substring includes the characters up to, but not including, the character indicated by end.
+     * If this value is not specified, the substring continues to the end of stringObj.
+     */
+    // slice(start?: number, end?: number): string;
+    String slice(Number? start = null, Number? end = null);
+
+    /**
+     * Split a string into substrings using the specified separator and return them as an array.
+     * @param separator A string that identifies character or characters to use in separating the string. If omitted, a single-element array containing the entire string is returned.
+     * @param limit A value used to limit the number of elements returned in the array.
+     */
+    // split(separator: string | RegExp, limit?: number): string[];
+    // TODO:
+
+    /**
+     * Returns the substring at the specified location within a String object.
+     * @param start The zero-based index number indicating the beginning of the substring.
+     * @param end Zero-based index number indicating the end of the substring. The substring includes the characters up to, but not including, the character indicated by end.
+     * If end is omitted, the characters from start through the end of the original string are returned.
+     */
+    // substring(start: number, end?: number): string;
+    String substring(Number start, Number? end = null);
+
+    /** Converts all the alphabetic characters in a string to lowercase. */
+    // toLowerCase(): string;
+    String toLowerCase();
+
+    /** Converts all alphabetic characters to lowercase, taking into account the host environment's current locale. */
+    // toLocaleLowerCase(locales?: string | string[]): string;
+    String toLocaleLowerCase(Union<String, Array<String>>? locales = null);
+
+    /** Converts all the alphabetic characters in a string to uppercase. */
+    // toUpperCase(): string;
+    String toUpperCase();
+
+    /** Returns a string where all alphabetic characters have been converted to uppercase, taking into account the host environment's current locale. */
+    // toLocaleUpperCase(locales?: string | string[]): string;
+    String toLocaleUpperCase(Union<String, Array<String>>? locales = null);
+
+    /** Removes the leading and trailing white space and line terminator characters from a string. */
+    // trim(): string;
+    String trim();
+
+    /** Returns the length of a String object. */
+    // readonly length: number;
+    Number length { get; }
+
+    /** Returns the primitive value of the specified object. */
+    // valueOf(): string;
+    String valueOf();
+
+    // readonly [index: number]: string;
+    String this[Number index] { get; }
+}
+
+public partial struct String
+{
+    /** Returns a string representation of a string. */
+    // toString(): string;
+    public String toString()
+         => (String)_value.Call(NameTable.toString);
+
+    /**
+     * Returns the character at the specified index.
+     * @param pos The zero-based index of the desired character.
+     */
+    // charAt(pos: number): string;
+    public String charAt(Number index)
+         => (String)_value.Call(NameTable.charAt, index);
+
+    /**
+     * Returns the Unicode value of the character at the specified location.
+     * @param index The zero-based index of the desired character. If there is no character at the specified index, NaN is returned.
+     */
+    // charCodeAt(index: number): number;
+    public String charCodeAt(Number index)
+         => (String)_value.Call(NameTable.charCodeAt, index);
+
+    //    /**
+    //     * Returns a string that contains the concatenation of two or more strings.
+    //     * @param strings The strings to append to the end of the string.
+    //     */
+    // concat(...strings: string[]): string;
+//    public String concat(params String[] strings);
+
+    // /**
+    //  * Returns the position of the first occurrence of a substring.
+    //  * @param searchString The substring to search for in the string
+    //  * @param position The index at which to begin searching the String object. If omitted, search starts at the beginning of the string.
+    //  */
+    // indexOf(searchString: string, position?: number): number;
+
+    // /**
+    //  * Returns the last occurrence of a substring in the string.
+    //  * @param searchString The substring to search for.
+    //  * @param position The index at which to begin searching. If omitted, the search begins at the end of the string.
+    //  */
+    // lastIndexOf(searchString: string, position?: number): number;
+
+    // /**
+    //  * Determines whether two strings are equivalent in the current locale.
+    //  * @param that String to compare to target string
+    //  */
+    // localeCompare(that: string): number;
+
+    // /**
+    //  * Matches a string with a regular expression, and returns an array containing the results of that search.
+    //  * @param regexp A variable name or string literal containing the regular expression pattern and flags.
+    //  */
+    // match(regexp: string | RegExp): RegExpMatchArray | null;
+
+    // /**
+    //  * Replaces text in a string, using a regular expression or search string.
+    //  * @param searchValue A string or regular expression to search for.
+    //  * @param replaceValue A string containing the text to replace. When the {@linkcode searchValue} is a `RegExp`, all matches are replaced if the `g` flag is set (or only those matches at the beginning, if the `y` flag is also present). Otherwise, only the first match of {@linkcode searchValue} is replaced.
+    //  */
+    // replace(searchValue: string | RegExp, replaceValue: string): string;
+
+    // /**
+    //  * Replaces text in a string, using a regular expression or search string.
+    //  * @param searchValue A string to search for.
+    //  * @param replacer A function that returns the replacement text.
+    //  */
+    // replace(searchValue: string | RegExp, replacer: (substring: string, ...args: any[]) => string): string;
+
+    // /**
+    //  * Finds the first substring match in a regular expression search.
+    //  * @param regexp The regular expression pattern and applicable flags.
+    //  */
+    // search(regexp: string | RegExp): number;
+
+    // /**
+    //  * Returns a section of a string.
+    //  * @param start The index to the beginning of the specified portion of stringObj.
+    //  * @param end The index to the end of the specified portion of stringObj. The substring includes the characters up to, but not including, the character indicated by end.
+    //  * If this value is not specified, the substring continues to the end of stringObj.
+    //  */
+    // slice(start?: number, end?: number): string;
+
+    // /**
+    //  * Split a string into substrings using the specified separator and return them as an array.
+    //  * @param separator A string that identifies character or characters to use in separating the string. If omitted, a single-element array containing the entire string is returned.
+    //  * @param limit A value used to limit the number of elements returned in the array.
+    //  */
+    // split(separator: string | RegExp, limit?: number): string[];
+
+    // /**
+    //  * Returns the substring at the specified location within a String object.
+    //  * @param start The zero-based index number indicating the beginning of the substring.
+    //  * @param end Zero-based index number indicating the end of the substring. The substring includes the characters up to, but not including, the character indicated by end.
+    //  * If end is omitted, the characters from start through the end of the original string are returned.
+    //  */
+    // substring(start: number, end?: number): string;
+
+    // /** Converts all the alphabetic characters in a string to lowercase. */
+    // toLowerCase(): string;
+
+    // /** Converts all alphabetic characters to lowercase, taking into account the host environment's current locale. */
+    // toLocaleLowerCase(locales?: string | string[]): string;
+
+    // /** Converts all the alphabetic characters in a string to uppercase. */
+    // toUpperCase(): string;
+
+    // /** Returns a string where all alphabetic characters have been converted to uppercase, taking into account the host environment's current locale. */
+    // toLocaleUpperCase(locales?: string | string[]): string;
+
+    // /** Removes the leading and trailing white space and line terminator characters from a string. */
+    // trim(): string;
+
+    // /** Returns the length of a String object. */
+    // readonly length: number;
+
+    // // IE extensions
+    // /**
+    //  * Gets a substring beginning at the specified location and having the specified length.
+    //  * @deprecated A legacy feature for browser compatibility
+    //  * @param from The starting position of the desired substring. The index of the first character in the string is zero.
+    //  * @param length The number of characters to include in the returned substring.
+    //  */
+    // substr(from: number, length?: number): string;
+
+    // /** Returns the primitive value of the specified object. */
+    // valueOf(): string;
+
+    // readonly [index: number]: string;
+}
+
+//interface StringConstructor {
+//    new(value?: any): String;
+//    (value?: any): string;
+//    readonly prototype: String;
+//    fromCharCode(...codes: number[]): string;
+//}
+
+///**
+// * Allows manipulation and formatting of text strings and determination and location of substrings within strings.
+// */
+//declare var String: StringConstructor;
+
 
 public class NameTable
 {
@@ -539,12 +815,40 @@ public class NameTable
     public static JSValue Object => GetStringName(nameof(Object));
     public static JSValue byteLength => GetStringName(nameof(byteLength));
     public static JSValue slice => GetStringName(nameof(slice));
+    public static JSValue charAt => GetStringName(nameof(charAt));
+    public static JSValue charCodeAt => GetStringName(nameof(charCodeAt));
+    public static JSValue concat => GetStringName(nameof(concat));
+    public static JSValue indexOf => GetStringName(nameof(indexOf));
+    public static JSValue lastIndexOf => GetStringName(nameof(lastIndexOf));
+    public static JSValue localeCompare => GetStringName(nameof(localeCompare));
+    public static JSValue match => GetStringName(nameof(match));
+    public static JSValue replace => GetStringName(nameof(replace));
+    public static JSValue search => GetStringName(nameof(search));
+    public static JSValue split => GetStringName(nameof(split));
+    public static JSValue substring => GetStringName(nameof(substring));
+    public static JSValue toLowerCase => GetStringName(nameof(toLowerCase));
+    public static JSValue toLocaleLowerCase => GetStringName(nameof(toLocaleLowerCase));
+    public static JSValue toUpperCase => GetStringName(nameof(toUpperCase));
+    public static JSValue toLocaleUpperCase => GetStringName(nameof(toLocaleUpperCase));
+    public static JSValue trim => GetStringName(nameof(trim));
+    public static JSValue length => GetStringName(nameof(length));
+    public static JSValue substr => GetStringName(nameof(substr));
+    public static JSValue fromCharCode => GetStringName(nameof(fromCharCode));
+
 
     // TODO: Implement
     public static JSValue GetStringName(string value) => JSValue.Null;
 }
 
-public struct Union<T1, T2, T3>
+public struct Union<T1, T2> : IJSValueHolder<Union<T1, T2>>
+{
+    private JSValue _value;
+
+    public static explicit operator Union<T1, T2>(JSValue value) => new Union<T1, T2> { _value = value };
+    public static implicit operator JSValue(Union<T1, T2> value) => value._value;
+}
+
+public struct Union<T1, T2, T3> : IJSValueHolder<Union<T1, T2, T3>>
 {
     private JSValue _value;
 
@@ -644,7 +948,7 @@ public struct Array<T>
         => value is Array<T> anyValue ? anyValue._value : JSValue.Undefined;
 }
 
-public struct String
+public partial struct String : IJSValueHolder<String>
 {
     private JSValue _value;
 
