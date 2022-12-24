69f0b89d2f4df63c69466619e4d398f8f6a4ec92
Vladimir Morozov <vmoroz@users.noreply.github.com>
2022-12-24 07:34:26 -0800

    Implement IObjectConstructor

--- Runtime/TypeScriptModel.cs
+++ Runtime/TypeScriptModel.cs
@@ -183,126 +183,22 @@ public partial interface IObjectConstructor<TSelf> : IJSValueHolder<TSelf>
     Object prototype { get; }
     Any getPrototypeOf(Any obj);
     PropertyDescriptor? getOwnPropertyDescriptor(Any obj, PropertyKey key);
+    Array<String> getOwnPropertyNames(Any obj);
+    Any create(Nullable<Object> obj);
+    Any create(Nullable<Object> obj, Intersect<PropertyDescriptorMap, ThisType<Any>> properties);
+    T defineProperty<T>(T obj, PropertyKey key, Intersect<PropertyDescriptor, ThisType<Any>> attributes) where T : struct, IJSValueHolder<T>;
+    T defineProperties<T>(T obj, Intersect<PropertyDescriptorMap, ThisType<Any>> properties) where T : struct, IJSValueHolder<T>;
+    T seal<T>(T obj) where T : struct, IJSValueHolder<T>;
+    Readonly<T> freeze<T>(T obj) where T : struct, IJSValueHolder<T>;
+    T preventExtensions<T>(T obj) where T : struct, IJSValueHolder<T>;
+    Boolean isSealed(Any obj);
+    Boolean isFrozen(Any obj);
+    Boolean isExtensible(Any obj);
+    Array<String> keys(Object obj);
 }
 
 public partial struct ObjectConstructor : IObjectConstructor<ObjectConstructor>
 {
-    /**
-     * Returns the names of the own properties of an object. The own properties of an object are those that are defined directly
-     * on that object, and are not inherited from the object's prototype. The properties of an object include both fields (objects) and functions.
-     * @param o Object that contains the own properties.
-     */
-    // getOwnPropertyNames(o: any): string[];
-    public Array<String> GetOwnPropertyNames(Any value)
-        => (Array<String>)_value.CallMethod(NameTable.getOwnPropertyNames, value);
-
-    /**
-     * Creates an object that has the specified prototype or that has null prototype.
-     * @param o Object to use as a prototype. May be null.
-     */
-    // create(o: object | null): any;
-    public Any Create(Nullable<Object> value)
-        => (Any)_value.CallMethod(NameTable.create, value);
-    /**
-     * Creates an object that has the specified prototype, and that optionally contains specified properties.
-     * @param o Object to use as a prototype. May be null
-     * @param properties JavaScript object that contains one or more property descriptors.
-     */
-    // create(o: object | null, properties: PropertyDescriptorMap & ThisType<any>): any;
-    public Any Create(Nullable<Object> value, Intersect<PropertyDescriptorMap, ThisType<Any>> properties)
-        => (Any)_value.CallMethod(NameTable.create, value, properties);
-
-    /**
-     * Adds a property to an object, or modifies attributes of an existing property.
-     * @param o Object on which to add or modify the property. This can be a native JavaScript object (that is, a user-defined object or a built in object) or a DOM object.
-     * @param p The property name.
-     * @param attributes Descriptor for the property. It can be for a data property or an accessor property.
-     */
-    // defineProperty<T>(o: T, p: PropertyKey, attributes: PropertyDescriptor & ThisType<any>): T;
-    public T DefineProperty<T>(T value, PropertyKey key, Intersect<PropertyDescriptor, ThisType<Any>> attributes)
-        where T : struct, IJSValueHolder<T>
-        => (T)_value.CallMethod(NameTable.defineProperty, value, key, attributes);
-    /**
-     * Adds one or more properties to an object, and/or modifies attributes of existing properties.
-     * @param o Object on which to add or modify the properties. This can be a native JavaScript object or a DOM object.
-     * @param properties JavaScript object that contains one or more descriptor objects. Each descriptor object describes a data property or an accessor property.
-     */
-    // defineProperties<T>(o: T, properties: PropertyDescriptorMap & ThisType<any>): T;
-    public T DefineProperties<T>(T value, Intersect<PropertyDescriptorMap, ThisType<Any>> properties)
-        where T : struct, IJSValueHolder<T>
-        => (T)_value.CallMethod(NameTable.defineProperties, value, properties);
-
-    /**
-     * Prevents the modification of attributes of existing properties, and prevents the addition of new properties.
-     * @param o Object on which to lock the attributes.
-     */
-    // seal<T>(o: T): T;
-    public T Seal<T>(T value)
-        where T : struct, IJSValueHolder<T>
-        => (T)_value.CallMethod(NameTable.seal, value);
-
-    /**
-     * Prevents the modification of existing property attributes and values, and prevents the addition of new properties.
-     * @param f Object on which to lock the attributes.
-     */
-    // freeze<T extends Function>(f: T): T;
-    //public T Freeze<T>(T value)
-    //    where T : struct, IFunction<T>
-    //    => (T)_value.CallMethod(NameTable.freeze, value);
-
-    /**
-     * Prevents the modification of existing property attributes and values, and prevents the addition of new properties.
-     * @param o Object on which to lock the attributes.
-     */
-    // freeze<T extends {[idx: string]: U | null | undefined | object}, U extends string | bigint | number | boolean | symbol>(o: T): Readonly<T>;
-
-    /**
-     * Prevents the modification of existing property attributes and values, and prevents the addition of new properties.
-     * @param o Object on which to lock the attributes.
-     */
-    // freeze<T>(o: T): Readonly<T>;
-    public Readonly<T> Freeze<T>(T value)
-        where T : struct, IJSValueHolder<T>
-        => (Readonly<T>)_value.CallMethod(NameTable.freeze, value);
-
-    /**
-     * Prevents the addition of new properties to an object.
-     * @param o Object to make non-extensible.
-     */
-    // preventExtensions<T>(o: T): T;
-    public T PreventExtensions<T>(T value)
-        where T : struct, IJSValueHolder<T>
-        => (T)_value.CallMethod(NameTable.preventExtensions, value);
-
-    /**
-     * Returns true if existing property attributes cannot be modified in an object and new properties cannot be added to the object.
-     * @param o Object to test.
-     */
-    // isSealed(o: any): boolean;
-    public Boolean IsSealed(Any value)
-        => (Boolean)_value.CallMethod(NameTable.isSealed, value);
-
-    /**
-     * Returns true if existing property attributes and values cannot be modified in an object, and new properties cannot be added to the object.
-     * @param o Object to test.
-     */
-    // isFrozen(o: any): boolean;
-    public Boolean IsFrozen(Any value)
-        => (Boolean)_value.CallMethod(NameTable.isFrozen, value);
-    /**
-     * Returns a value that indicates whether new properties can be added to an object.
-     * @param o Object to test.
-     */
-    // isExtensible(o: any): boolean;
-    public Boolean IsExtensible(Any value)
-        => (Boolean)_value.CallMethod(NameTable.isExtensible, value);
-    /**
-     * Returns the names of the enumerable string properties and methods of an object.
-     * @param o Object that contains the properties and methods. This can be an object that you created or an existing Document Object Model (DOM) object.
-     */
-    // keys(o: object): string[];
-    public Array<String> Keys(Object value)
-        => (Array<String>)_value.CallMethod(NameTable.keys, value);
 }
 
 public partial interface IGlobal<TSelf>
@@ -619,17 +515,6 @@ public partial struct ArrayBuffer : IJSValueHolder<ArrayBuffer>
 
 public partial class NameTable
 {
-    public static JSValue getOwnPropertyNames => GetStringName(nameof(getOwnPropertyNames));
-    public static JSValue create => GetStringName(nameof(create));
-    public static JSValue defineProperty => GetStringName(nameof(defineProperty));
-    public static JSValue defineProperties => GetStringName(nameof(defineProperties));
-    public static JSValue seal => GetStringName(nameof(seal));
-    public static JSValue freeze => GetStringName(nameof(freeze));
-    public static JSValue preventExtensions => GetStringName(nameof(preventExtensions));
-    public static JSValue isSealed => GetStringName(nameof(isSealed));
-    public static JSValue isFrozen => GetStringName(nameof(isFrozen));
-    public static JSValue isExtensible => GetStringName(nameof(isExtensible));
-    public static JSValue keys => GetStringName(nameof(keys));
     public static JSValue byteLength => GetStringName(nameof(byteLength));
     public static JSValue slice => GetStringName(nameof(slice));
     public static JSValue charAt => GetStringName(nameof(charAt));
