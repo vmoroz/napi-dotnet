9532cbada83de9c4d54b3ee9f0811bcea9d9737b
Vladimir Morozov <vmoroz@users.noreply.github.com>
2022-12-27 11:28:54 -0800

    Extract ExtensionMethods

---
+++ Runtime/TypedModel/ExtensionMethods.cs
@@ -0,0 +1,70 @@
+using System.Linq;
+
+namespace NodeApi.TypedModel;
+
+public static class ExtensionMethods
+{
+    public static JSValue CallMethod(this JSValue thisValue, JSValue name)
+        => thisValue.GetProperty(name).Call(thisValue);
+
+    public static JSValue CallMethod(this JSValue thisValue, JSValue name, JSValue arg0)
+        => thisValue.GetProperty(name).Call(thisValue, arg0);
+
+    public static JSValue CallMethod(this JSValue thisValue, JSValue name, JSValue arg0, JSValue arg1)
+        => thisValue.GetProperty(name).Call(thisValue, arg0, arg1);
+
+    public static JSValue CallMethod(this JSValue thisValue, JSValue name, JSValue arg0, JSValue arg1, JSValue arg2)
+        => thisValue.GetProperty(name).Call(thisValue, arg0, arg1, arg2);
+
+    public static JSValue CallMethod(this JSValue thisValue, JSValue name, params JSValue[] args)
+        => thisValue.GetProperty(name).Call(thisValue, args);
+
+    public static JSValue CallMethod<T>(this JSValue thisValue, JSValue name, params T[] args)
+        where T : struct, IJSValueHolder<T>
+            => thisValue.GetProperty(name).Call(thisValue, args.Select(a => (JSValue)a).ToArray());
+
+    public static JSValue CallMethod<T>(this JSValue thisValue, JSValue name, JSValue arg0, params T[] args)
+        where T : struct, IJSValueHolder<T>
+        => thisValue.GetProperty(name).Call(thisValue, arg0, args.Select(a => (JSValue)a).ToArray());
+
+    public static JSValue CallMethod<T>(this JSValue thisValue, JSValue name, JSValue arg0, JSValue arg1, params T[] args)
+        where T : struct, IJSValueHolder<T>
+        => thisValue.GetProperty(name).Call(thisValue, arg0, arg1, args.Select(a => (JSValue)a).ToArray());
+
+    public static JSValue CallMethod<T>(this JSValue thisValue, JSValue name, JSValue arg0, JSValue arg1, JSValue arg2, params T[] args)
+        where T : struct, IJSValueHolder<T>
+        => thisValue.GetProperty(name).Call(thisValue, arg0, arg1, arg2, args.Select(a => (JSValue)a).ToArray());
+
+    public static JSValue Call<T>(this JSValue thisValue, params T[] args)
+        where T : struct, IJSValueHolder<T>
+        => JSNativeApi.Call(thisValue, thisValue, args.Select(a => (JSValue)a).ToArray());
+
+    public static JSValue Call<T>(this JSValue thisValue, JSValue arg0, params T[] args)
+        where T : struct, IJSValueHolder<T>
+        => JSNativeApi.Call(thisValue, thisValue, arg0, args.Select(a => (JSValue)a).ToArray());
+
+    public static JSValue Call<T>(this JSValue thisValue, JSValue arg0, JSValue arg1, params T[] args)
+        where T : struct, IJSValueHolder<T>
+        => JSNativeApi.Call(thisValue, thisValue, arg0, arg1, args.Select(a => (JSValue)a).ToArray());
+
+    public static JSValue Call<T>(this JSValue thisValue, JSValue arg0, JSValue arg1, JSValue arg2, params T[] args)
+        where T : struct, IJSValueHolder<T>
+        => JSNativeApi.Call(thisValue, thisValue, arg0, arg1, arg2, args.Select(a => (JSValue)a).ToArray());
+
+    public static JSValue CallAsConstructor<T>(this JSValue thisValue, params T[] args)
+    where T : struct, IJSValueHolder<T>
+        => JSNativeApi.CallAsConstructor(thisValue, args.Select(a => (JSValue)a).ToArray());
+
+    public static JSValue CallAsConstructor<T>(this JSValue thisValue, JSValue arg0, params T[] args)
+        where T : struct, IJSValueHolder<T>
+        => JSNativeApi.CallAsConstructor(thisValue, arg0, args.Select(a => (JSValue)a).ToArray());
+
+    public static JSValue CallAsConstructor<T>(this JSValue thisValue, JSValue arg0, JSValue arg1, params T[] args)
+        where T : struct, IJSValueHolder<T>
+        => JSNativeApi.CallAsConstructor(thisValue, arg0, arg1, args.Select(a => (JSValue)a).ToArray());
+
+    public static JSValue CallAsConstructor<T>(this JSValue thisValue, JSValue arg0, JSValue arg1, JSValue arg2, params T[] args)
+        where T : struct, IJSValueHolder<T>
+        => JSNativeApi.CallAsConstructor(thisValue, arg0, arg1, arg2, args.Select(a => (JSValue)a).ToArray());
+}
+
--- Runtime/TypedModel/NameTable.cs
+++ Runtime/TypedModel/NameTable.cs
@@ -1,6 +1,3 @@
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
 namespace NodeApi.TypedModel;
 
 // Keeps a list of pre-allocated strings
