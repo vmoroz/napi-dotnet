46530f7eda9798b85bd0f41c90979b85fa9adaa4
Vladimir Morozov <vmoroz@users.noreply.github.com>
2022-12-29 05:31:30 -0800

    Extract JSObjectPropertyEnumerator

--- Runtime/JSValue.cs
+++ Runtime/JSValue.cs
@@ -108,82 +108,14 @@ public struct JSValue
             _value = value;
         }
 
-        public PropertyEnumerator GetEnumerator()
-            => new PropertyEnumerator(_value);
+        public JSObjectPropertyEnumerator GetEnumerator()
+            => new JSObjectPropertyEnumerator(_value);
 
         IEnumerator<(JSValue name, JSValue value)> IEnumerable<(JSValue name, JSValue value)>.GetEnumerator()
-            => new PropertyEnumerator(_value);
+            => new JSObjectPropertyEnumerator(_value);
 
         IEnumerator IEnumerable.GetEnumerator()
-            => new PropertyEnumerator(_value);
-    }
-
-    public struct PropertyEnumerator : IEnumerator<(JSValue name, JSValue value)>, IEnumerator
-    {
-        private readonly JSValue _value;
-        private readonly JSValue _names;
-        private readonly int _count;
-        private int _index;
-        private (JSValue name, JSValue value)? _current;
-
-        internal PropertyEnumerator(JSValue value)
-        {
-            _value = value;
-            JSValueType valueType = value.TypeOf();
-            if (valueType == JSValueType.Object || valueType == JSValueType.Function)
-            {
-                JSValue names = value.GetPropertyNames();
-                _names = names;
-                _count = names.GetArrayLength();
-            }
-            else
-            {
-                _names = Undefined;
-                _count = 0;
-            }
-            _index = 0;
-            _current = default;
-        }
-
-        public void Dispose()
-        {
-        }
-
-        public bool MoveNext()
-        {
-            if (_index < _count)
-            {
-                JSValue name = _names.GetElement(_index);
-                _current = (name, _value.GetProperty(name));
-                _index++;
-                return true;
-            }
-
-            _index = _count + 1;
-            _current = default;
-            return false;
-        }
-
-        public (JSValue name, JSValue value) Current
-            => _current ?? throw new InvalidOperationException("Unexpected enumerator state");
-
-        object? IEnumerator.Current
-        {
-            get
-            {
-                if (_index == 0 || _index == _count + 1)
-                {
-                    throw new InvalidOperationException("Invalid enumerator state");
-                }
-                return Current;
-            }
-        }
-
-        void IEnumerator.Reset()
-        {
-            _index = 0;
-            _current = default;
-        }
+            => new JSObjectPropertyEnumerator(_value);
     }
 
     public struct EnumerableItems : IEnumerable<JSValue>, IEnumerable
